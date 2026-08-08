bdc72e6cda91163ffb0cfdccadb9f8bc5f6f4788
rishi-bh <rishibhargava729@gmail.com>
2026-08-08 16:15:08 -0400

    Implement Black-Scholes call price function

---
+++ models/black_scholes.py
@@ -0,0 +1,4 @@
+def call_price(S0, K, r, sigma, T):
+    d1 = (np.log(S0 / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return S0 * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
