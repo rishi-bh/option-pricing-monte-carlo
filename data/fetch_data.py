94bf1e3de7414ef36dcf9ae67245728bb1e5a705
rishi-bh <rishibhargava729@gmail.com>
2026-08-09 19:58:50 -0400

    Add function to calculate annualized volatility
    Refactored code to encapsulate annualized volatility calculation in a function.

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -1,22 +1,16 @@
 import yfinance as yf
+def get_annualized_volatility():
+    data = yf.download("SPY", start = "2024-01-01", end = "2026-01-01", auto_adjust= True)
+    
+    prices = data["Close"]
+    
+    log_returns = np.log(
+        prices / prices.shift(1)
+    ).dropna()
 
-data = yf.download("SPY", start = "2024-01-01", end = "2026-01-01", auto_adjust= True)
-
-print(data.head())
-
-prices = data["Close"]
-
-log_returns = np.log(
-    prices / prices.shift(1)
-).dropna()
-
-print(log_returns.head())
-
-daily_volatility = log_returns.std()
-
-annualized_volatility = daily_volatility * np.sqrt(252)
-
-print(
-    f"Annualized volatility: "
-    f"{annualized_volatility.item():.2%}"
-)
+    
+    daily_volatility = log_returns.std()
+    
+    annualized_volatility = daily_volatility * np.sqrt(252)
+    
+    return float(annualized_volatility)
