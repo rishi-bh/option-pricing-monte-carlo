e2e58f900f950596bcc223a1fd27926747a3071c
rishi-bh <rishibhargava729@gmail.com>
2026-08-09 19:56:25 -0400

    Rename call_price to black_scholes_call

--- models/black_scholes.py
+++ models/black_scholes.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def call_price(S0, K, r, sigma, T):
+def black_scholes_call(S0, K, r, sigma, T):
     d1 = (np.log(S0 / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     return S0 * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
